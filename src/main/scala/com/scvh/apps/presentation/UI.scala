c04df32ba67baa92d2c2e074eb6d31cccc6de7c3
schvabodka-man <scvhapps@gmail.com>
2017-07-28 17:20:20 +0300

    starting implementing css

--- src/main/scala/com/scvh/apps/presentation/UI.scala
+++ src/main/scala/com/scvh/apps/presentation/UI.scala
@@ -1,8 +1,19 @@
 package com.scvh.apps.presentation
 
+import com.scvh.apps.application.brainresults.JsonAnswer
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class UI {
 
+  @RequestMapping(Array("/"))
+  def mainPage(): String = {
+    "index"
+  }
+
+  @RequestMapping(Array("/update"))
+  def processingBrainfuck: JsonAnswer = {
+    null
+  }
 }
