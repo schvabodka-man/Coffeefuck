8dccea6e4de65f6d29cde0a94234d0859c87d551
schvabodka-man <scvhapps@gmail.com>
2017-08-10 14:14:47 +0300

    This fucking icon + more fancy html

--- src/main/scala/com/scvh/apps/application/brainfuck/BrainfuckBundle.scala
+++ src/main/scala/com/scvh/apps/application/brainfuck/BrainfuckBundle.scala
@@ -1,15 +1,9 @@
 package com.scvh.apps.application.brainfuck
 
 import com.scvh.apps.application.brainfuck.brainruntime.{BrainfuckLoopsParameters, BrainfuckMachineParameters, BrainfuckRuntime}
-import org.springframework.beans.factory.annotation.Autowired
 
 class BrainfuckBundle(val parameters: BrainfuckMachineParameters) {
-  var runtime: BrainfuckRuntime = new BrainfuckRuntime
-  var loops: BrainfuckLoopsParameters = new BrainfuckLoopsParameters
 
-  @Autowired
-  def injRuntime(runtime: BrainfuckRuntime) = this.runtime = runtime
-
-  @Autowired
-  def injLoopParams(loops: BrainfuckLoopsParameters) = this.loops = loops
+  var runtime: BrainfuckRuntime = new BrainfuckRuntime()
+  var loops: BrainfuckLoopsParameters = new BrainfuckLoopsParameters()
 }
